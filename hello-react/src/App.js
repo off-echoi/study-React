d6a7274664ce2068c7aa534cfd0c3c8396fca8fa
off-echoi <off.echoi@gmail.com>
2019-06-04 15:14:00 +0900

    JSX basic

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React , {Fragment} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const text = "Hello React";
+  const condition = true;
+  const style = {
+    backgroundColor:'gray',
+    border:'1px solid black',
+    height: Math.round(Math.random()*300) + 50,
+    width: Math.floor(Math.random()*300)+44,
+    WebKitTransition:'all',
+    transition:'all',
+    marginTop:'40px'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="my-div">
+      {/* 요소 밖 주석 */}
+      <h4>aaaaaa</h4>
+      <h6>{text}</h6>
+      {condition && '보여주시뇨'}
+      <div style={style}
+      // 요소 안 주석
+      /* 또한 이렇게 */
+      />
+      {/* 위의 것 처럼 self-close 요소 안에 주석 */}
+        /* 출력 */
+        // 출력
     </div>
   );
 }
