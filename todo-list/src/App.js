fb1283ab6139789168b57fb36e209aa37edc28ea
off-echoi <off.echoi@gmail.com>
2019-06-05 11:31:08 +0900

    practice React

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import MyComponent from './MyComponent';
+import EventPractice from './EventPractice';
 
 class App extends Component {
   render() {
     return (
       // 문자열 이외의 값을 컴포넌트에 전달할 때는 {}로 감싸야 함
       // 문자열은 name ="" 문자열은 {} 안에 담길 수 없다.
-        <MyComponent name="React" age={3}/>
+      //  <MyComponent name="React" age={3}/>
       
         // <MyComponent name={2} age={3}/>
         // 이 경우 출력은 되지만 proptypes.string 으로 인해 콘솔창에 에러 발생
         
+        <EventPractice/>
+
     );
   }
 }
@@ -21,7 +24,7 @@ export default App;
 // import MyComponent from './MyComponent';
 
 // function App() {
-//     return (
+//     return ( 
 //       
 //        <MyComponent name="react"></MyComponent>
 //     );
--- todo-list/src/Mycomponent.js
+++ todo-list/src/Mycomponent.js
@@ -10,6 +10,10 @@ class MyComponent extends Component {
         name : PropTypes.string,
         age : PropTypes.number.isRequired
     }
+  
+    state = {
+        number : 0
+    }
 
     render() {
         return (
@@ -17,6 +21,13 @@ class MyComponent extends Component {
                 {/* props = 컴포넌트 속성을 설정할 때 사용. 부모 컴포넌트에서 사용 가능(현재로 치면 App.js) */}
                <p>안녕하세요. 제 이름은 {this.props.name} 입니다.</p>
                <p>제 나이는 {this.props.age} 입니다.</p>
+               <p>숫자 : {this.state.number}</p>
+               <button onClick={() => {
+                   this.setState({
+                       number: this.state.number +1
+                   })
+               }
+            }>더하기</button>
             </div>
         );
     }
