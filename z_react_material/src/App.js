79ef6a14436a126b4e64a80997fda12fcfa9041d
off-echoi <off.echoi@gmail.com>
2019-06-05 16:50:53 +0900

    react with material

--- z_react_material/src/App.js
+++ z_react_material/src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './customer'
 import './App.css';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = theme => ({
+  root:{
+    width:'100%',
+    matginTop:theme.spacing *3,
+    overflowX:'auto'
+  },
+  table:{
+    minWidth:1080
+  }
+});
+
+const customers = [
+  {
+    'id' : 1,
+    'name': '홍길동',
+    'dirthday':'961111',
+    'gender' : '남자',
+    'job': '대학생'
+  },
+  {
+    'id' : 2,
+    'name': '홍망동',
+    'dirthday':'911111',
+    'gender' : '남자',
+    'job': '대학생2'
+  }
+]
+
+class App extends Component {
+  render(){
+    const { classes } = this.props;
+    return (
+      <div className = {classes.root}>
+          <Table className = {classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {customers.map(c=>{
+                  return <Customer key={c.id} id={c.id} name={c.name} dirthday={c.birthday} gender={c.gender} job={c.job}/>
+              })}
+            </TableBody>
+
+          </Table>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
