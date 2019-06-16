d6189a4b1dc91aeb211e05be2f6c462deb3f8e66
echo <off.echoi@gmail.com>
2019-06-16 22:55:00 +0900

    Ra ex

--- todo-list/src/EventPractice.js
+++ todo-list/src/EventPractice.js
@@ -5,34 +5,35 @@ class EventPractice extends Component {
     state = {
         message : ''
     }
+    constructor(props){
+        super(props);
+        this.handleChnage = this.handleChnage.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChnage(e){
+        this.setState({
+            message:e.target.value
+        });
+    }
+    handleClick(){
+        alert(this.state.message);
+        this.setState({
+            message:''
+        })
+    }
     render() {
         return (
             <div>
                 <h1>이벤트 연습</h1>
-                <input  
-                type = "text"
-                name="message"
-                placeholder="아무거나"
-                value ={this.state.message}
-                onChange={
-                    (e) => {
-                        this.setState({
-                            message: e.target.value
-                            
-                        })
-
-                    }
-                }
+                <input
+                    type="text"
+                    name="message"
+                    placeholder="아무거나"
+                    value={this.state.message}
+                    onChange={this.handleChnage}
                 />
-                <button onClick={ 
-                    () => {
-                            alert(this.state.message);
-                            this.setState({
-                                message:''
-                            })
-                        }
-                    }
-                >확인</button>
+                <button onClick={this.handleClick}>확인</button>
             </div>
         );
     }
