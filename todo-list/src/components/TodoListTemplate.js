65a59d4d9394150b6d5bea4af63588e31db0e20b
echo <off.echoi@gmail.com>
2019-06-25 00:51:26 +0900

    todolist

--- todo-list/src/components/TodoListTemplate.js
+++ todo-list/src/components/TodoListTemplate.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './TodoListTemplate.css'
 
-const TodoListTemplate = ({form,children}) => {
+const TodoListTemplate = ({forms,children}) => {
     //  TodoListTemplate 에서는 form 값과 children 값을 받아온다.!
     return(
         <main className="todo-list-template">
@@ -9,7 +9,7 @@ const TodoListTemplate = ({form,children}) => {
                 오늘 할 일
             </div>
             <section className="form-wrapper">
-                {form}
+                {forms}
             </section>
             <section className="todos-wrapper">
                 {children}
