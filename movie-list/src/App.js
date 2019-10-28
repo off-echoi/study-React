17299af418197610d1be6df94c8398548c248fdf
ecchoi <off.echoi@gmail.com>
2019-10-28 15:17:40 +0900

    merge

--- movie-list/src/App.js
+++ movie-list/src/App.js
@@ -4,23 +4,23 @@ import Movie from './Movie'
 
 const movies =[
   {
-    title : 'AAA',
+    title : 'AAAA',
     poster : 'http://momsmagazine.co.kr/worp/wp-content/uploads/2015/08/%EB%B2%A0%ED%85%8C%EB%9E%91.jpg'
   },
   {
-    title : 'BBB',
+    title : 'BBBB',
     poster : 'https://movie-simg.yes24.com/NYes24/MOVIE/M90/M52/M00006905255_91952.jpg'
   },
   {
-    title : 'CCC',
+    title : 'CCCC',
     poster : 'http://www.rivalnews.co.kr/news/photo/201608/1154_1012_832.jpg'
   },
   {
-    title : 'DDD',
+    title : 'DDDD',
     poster : 'http://www.dydailynews.com/imgdata/dydailynews_com/201808/2018082716567091.jpg'
   },
   {
-    title : 'EEE',
+    title : 'EEEE',
     poster : 'http://www.dailyt.co.kr/news/photo/201901/34949_19672_3125.jpg'
   },
 
