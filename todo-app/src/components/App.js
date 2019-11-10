2bf0daebadd3956e068775ec2dc4f086db4e6ab3
ecchoi <off.echoi@gmail.com>
2019-11-10 23:56:35 +0900

    pageTemplate css 작성

--- todo-app/src/components/App.js
+++ todo-app/src/components/App.js
@@ -4,7 +4,7 @@ import PageTemplate from './PageTemplate';
 class App extends Component {
     render() {
         return (
-            <PageTemplate>안녕하세요</PageTemplate>
+            <PageTemplate></PageTemplate>
         );
     }
 }
